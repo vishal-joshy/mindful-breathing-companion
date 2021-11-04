a5a3fddf8c15718e5066aec7a12adcb3ba908f56
VishalJoshy <1812-vishal@protonmail.com>
2021-11-04 17:18:29 +0530

    Remove NavBar collapse Make Nav item names shorter

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 function NavBar() {
+	const isDesktopDevice = useMediaQuery({
+		query: "(min-device-width: 900px)",
+	});
+	console.log({ isDesktopDevice });
 	return (
 		<div>
 			<Navbar bg='light' expand='lg'>
-				<Container>
+				<Container
+					style={{
+						display: "flex",
+						flexDirection: isDesktopDevice ? "row" : "column",
+						alignItems: "flex-start",
+					}}>
 					<Navbar.Brand>Mindful Breathing</Navbar.Brand>
-					<Navbar.Toggle aria-controls='basic-navbar-nav' />
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='me-auto'>
-						
-							<Nav.Link href='#/box'>Box Breathing</Nav.Link>
+					<Navbar style={{width:isDesktopDevice?'auto':'100%'}}>
+						<Nav
+							style={{
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "flex-start",
+								flex: "1",
+								justifyContent: "space-around",
+							}}>
+							<Nav.Link href='#/box'>Box </Nav.Link>
 
-							<Nav.Link href='#/coherent'>Coherent Breathing</Nav.Link>
+							<Nav.Link href='#/coherent'>Coherent </Nav.Link>
 
-							<Nav.Link href='#/four-seven'>4-7-8 breathing</Nav.Link>
+							<Nav.Link href='#/four-seven'>4-7-8 </Nav.Link>
 						</Nav>
-					</Navbar.Collapse>
+					</Navbar>
 				</Container>
 			</Navbar>
 		</div>
