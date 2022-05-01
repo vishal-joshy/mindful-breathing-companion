366932fa250f490e32f6b5f726be5e26e267489d
vishaljoshy18 <1812-vishal@protonmail.com>
2022-05-01 21:14:53 +0530

    Refactor.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import logo from "./logo.svg";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter, Switch, Route, Link } from "react-router-dom";
-import Landing from "./components/Main/Landing";
-import BoxBreathing from "./components/Exercises/BoxBeathing/BoxBreathing";
-import CoherentBreathing from "./components/Exercises/CoherentBreathing/CoherentBreathing";
-import FourSevenBreathing from "./components/Exercises/FourSevenBreathing/FourSevenBreathing";
-import NavBar from "./components/Header/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import NavBar from './components/Header/NavBar';
+import boxVideo from './assets/box-vid.mp4';
+import coherentVideo from './assets/coherent-vid.mp4';
+import fourSevenVideo from './assets/four-seven-vid.mp4';
+import VideoContainer from './components/VideoContainer';
+
+function VideoDisplay({ video }) {
+  return <VideoContainer video={video} />;
+}
 
 function App() {
-	return (
-		<>
-			<HashRouter>
-				<NavBar />
+  return (
+    <>
+      <HashRouter>
+        <NavBar />
 
-				<Switch>
-					<Route exact path='/' component={Landing} />
-					<Route exact path='/box' component={BoxBreathing} />
-					<Route exact path='/coherent' component={CoherentBreathing} />
-					<Route exact path='/four-seven' component={FourSevenBreathing} />
-				</Switch>
-			</HashRouter>
-		</>
-	);
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            exact
+            path="/box"
+            component={() => <VideoDisplay video={boxVideo} />}
+          />
+          <Route
+            exact
+            path="/coherent"
+            component={() => <VideoDisplay video={coherentVideo} />}
+          />
+          <Route
+            exact
+            path="/four-seven"
+            component={() => <VideoDisplay video={fourSevenVideo} />}
+          />
+        </Switch>
+      </HashRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/Exercises/FourSevenBreathing/FourSevenBreathing.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EmbedVideo from "../../EmbedVideo";
-import fourSevenVid from "./four-seven-vid.mp4";
-
-function FourSevenBreathing() {
-	return <EmbedVideo video={fourSevenVid} />;
-}
-
-export default FourSevenBreathing;
